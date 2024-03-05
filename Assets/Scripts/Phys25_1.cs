e360c41d35fc1d4da5f8c6735d0a038ce4a7a604
wxdnesday <todayis.wednesday111@gmail.com>
2024-03-05 15:15:52 -0800

    Added some character models and fixed first quiz bug

---
+++ Assets/Scripts/Phys25_1.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Phys25_1 : MonoBehaviour
+{
+    public void scenePhys25_1()
+    {
+        SceneManager.LoadScene("Phys25_1");
+    }
+}
